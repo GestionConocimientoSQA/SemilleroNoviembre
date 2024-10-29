1145b0661181a019f3d0783a50c82cafad702317
Kely Gonzales <kely.gonzalez@sqasa.co>
2024-10-29 16:41:34 -0500

    repositorio completo SemilleroNoviembre

---
+++ src/main/java/rafael/Clase1.java
@@ -0,0 +1,28 @@
+package rafael;
+/*
+ * @(#) Clase1.java 11.0
+ * Copyright 2023 SQA. Todos los derechos reservados.
+ */
+
+/**
+ * @autor Rafael Chica
+ * @Fecha: --o--
+ */
+public class Clase1 {
+    public static void main(String[] args) {
+        int num=2;
+        String cadena="";
+        float num2=0.1f;
+        boolean logical=true;
+
+        if (num<num2){
+
+            System.out.println("num2 si es mayor que num");
+
+        } else if (num>num2) {
+            System.out.println("num si es mayor que num2");
+        }
+
+
+    }
+}
